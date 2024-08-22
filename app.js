8e15c9410e3fb380f0388022fa13b28c2e49a64f
tevesgithub <vjteves@gmail.com>
2024-08-22 11:17:24 -0600

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const mongoose = require('mongoose');
 const app = express();
 require ('dotenv').config();
+const authRoutes = require('./routes/authRoutes.js');
+const studentRoutes = require ('./routes/studentRoutes.js');
 const PORT = process.env.PORT ||  3000;
 
 //connect to mongoDB
@@ -19,6 +21,10 @@ app.set('views', './views');
 
 //middlewares
 app.use(express.static('public'));
+app.use('/', authRoutes);
+app.use('/', studentRoutes);
+
+
 
 app.use((err, res, req, next) =>{
     console.error(err.stack);
