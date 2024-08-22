1d604589b06612f20d54d6f3d1f72067d1653969
tevesgithub <vjteves@gmail.com>
2024-08-22 11:42:43 -0600

    update

---
+++ controllers/authController.js
@@ -0,0 +1,4 @@
+const bcrypt = require ('bcrypt');
+const jwt = require ('jsonwebtoken');
+const AttendanceManager = require('../models/attendanceManager.js');
+require('dotenv').config();
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,35 @@
 const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes.js');
+const studentRoutes = require('./routes/studentRoutes.js');
+const PORT = process.env.PORT || 3000;
 
 
-router.get('/login', (req,res) =>{
-    res.render('login');
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => { console.log('Connected to MongoDB Database') })
+.catch((err) => { console.log(`Error connecting to database: ${err}`) });
 
+//View engines
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//Middlewares
+app.use(express.static('public'));
+app.use('/', authRoutes);
+app.use('/', studentRoutes);
+
+app.use((err, res, req, next) =>{
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+    next();
 });
 
-router.get('/register', (req,res) =>{
-    res.render('register');
 
+
+//Start server
+app.listen(PORT, () =>{
+    console.log(`Connected to port ${PORT}`);
 });
-
-
-
-
-
-
-
-module.exports = router;
